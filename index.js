6efd2f1c4a6f30c39ca9d97b5cb035656aaa7ca4
ECWireless <Elliott@valtechcreative.com>
2019-10-28 13:00:05 -0400

    Day 1 Testing

--- index.js
+++ index.js
@@ -4,17 +4,29 @@ import {
   StyleSheet,
   Text,
   View,
+  VrButton,
 } from 'react-360';
 
 export default class Hello360 extends React.Component {
+  state = {
+    count: 0,
+  };
+  
+  // This method increments our count, triggering a re-render
+  _incrementCount = () => {
+    this.setState({count: this.state.count + 1});
+  };
+  
   render() {
     return (
       <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            StudioME's VR Podcast
-          </Text>
-        </View>
+			<VrButton
+				onClick={this._incrementCount}
+				style={styles.greetingBox}>
+			<Text style={styles.greeting}>
+				{`Count: ${this.state.count}`}
+			</Text>
+			</VrButton>
       </View>
     );
   }
@@ -31,8 +43,8 @@ const styles = StyleSheet.create({
   },
   greetingBox: {
     padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
+    backgroundColor: '#8dcbc0',
+    borderColor: '#b6ce61',
     borderWidth: 2,
   },
   greeting: {
